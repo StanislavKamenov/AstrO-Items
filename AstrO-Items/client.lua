fc75be1dd8c9cf5493391fe08f0c35fc33e74e3a
AstrO <76092821+StanislavKamenov@users.noreply.github.com>
2023-05-03 20:09:37 +0300

    Add files via upload

---
+++ AstrO-Items/client.lua
@@ -0,0 +1,76 @@
+local QBCore = exports['qb-core']:GetCoreObject
+
+QBCore.Functions.CreateUseableItem('empty_armor', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('empty_bulletspistol', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('empty_bulletssmg', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('empty_bulletsrifle', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('empty_bulletsshotgun', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('armor_plates', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('gunpowder', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('pistol_body', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('pistol_trigger', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('pistol_magazine', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('pistol_barrel', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('switchblade_part1', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('switchblade_part2', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('pump_body', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('pump_magazine', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('pump_barrel', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('pump_trigger', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('smg_body', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('smg_magazine', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('smg_barrel', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('smg_trigger', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('assault_body', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('assault_magazine', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('assault_barrel', function(source)
+end)
+
+QBCore.Functions.CreateUseableItem('assault_trigger', function(source)
+end)
---
+++ AstrO-Items/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AstrO'
+description 'Simple Script For Items'
+version '1.0'
+
+
+server_scripts {
+	'server.lua',
+}
+
+exports {
+	'CreateUsableItem',
+} 
+
+lua54 'yes'
